1b792ca1d7593c090d4254678a604342b14a9631
AidanMcGregor <amcgrego@trinity.edu>
2019-09-05 11:12:34 -0500

    Added a virus

---
+++ src/main/scala/drmario/Board.scala
@@ -0,0 +1,8 @@
+package drmario
+
+class Board {
+    private var _elements = List[BoardElement](new Virus(2, 2, DMColor.Red))
+
+    def elements = _elements
+
+}
--- src/main/scala/drmario/Main.scala
+++ src/main/scala/drmario/Main.scala
@@ -5,11 +5,14 @@ import scalafx.scene.canvas.Canvas
 
 
 object Main extends JFXApp {
+    val canvas = new Canvas(800, 800)
+    val renderer = new Renderer(canvas)
+    val board = new Board
     stage = new JFXApp.PrimaryStage {
         title = "Dr. Mario"
         scene = new Scene(800, 800) {
-            val canvas = new Canvas(800, 800)
             content = canvas
+            renderer.render(board)
 
         }
     }
